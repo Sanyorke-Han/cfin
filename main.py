0e0f059ec2a6ed7aac02dd4db767a89689b2e0ac
Sanyorke Han <sanyorke@126.com>
2023-02-22 16:23:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+import yfinance as yf
 
 app = FastAPI()
 
@@ -16,3 +17,8 @@ async def health_check():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/stock/{ticker}")
+def get_stock(ticker: str):
+    df = yf.download(ticker, period='max')
+    return df.to_json()
