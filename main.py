4aa0564e8779d26261ee130f447cf90b322a00a1
Sanyorke Han <sanyorke@126.com>
2023-02-22 15:54:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/health")
+async def health_check():
+    return {"status": "Ok!"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
